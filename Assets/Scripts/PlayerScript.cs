779f14783c597b1e6c1262dd4bb0e25f25f82c7e
TechnoWizard42 <hartley.grim@knights.ucf.edu>
2021-10-20 18:41:47 -0400

    first challenge init

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,11 +13,40 @@ public class PlayerScript : MonoBehaviour
 
     private int scoreValue = 0;
 
+    private int livesValue = 3;
+
+    public Text lives;
+
+    public Text winText;
+
+    public Text loseText;
+
+    private bool facingRight = true;
+
+    Animator anim;
+
+    public AudioClip musicClipOne;
+
+    public AudioSource musicSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        scoreValue = 0;
+        livesValue = 3;
+        SetWinText();
+        winText.text = "";
+        SetLoseText();
+        loseText.text = "";
+
+        anim = GetComponent<Animator>();
+
     }
 
     void Update()
@@ -26,14 +55,61 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
     }
 
-// Update is called once per frame
-void FixedUpdate()
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    // PHYSICS AND UI BASED CODE PASSED THIS POINT
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -43,8 +119,49 @@ void FixedUpdate()
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            SetWinText();
         }
 
+        else if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            SetLoseText();
+        }
+
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector2(33f, 2f);
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+        }
+
+        if (livesValue == 0)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (scoreValue == 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void SetWinText()
+    {
+        if (scoreValue >= 8)
+        {
+            winText.text = "You won Hartley's Platformer!";
+        }
+    }
+
+    void SetLoseText()
+    {
+        if (livesValue <= 0)
+        {
+            loseText.text = "You lost Hartley's Platformer...";
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -57,5 +174,4 @@ void FixedUpdate()
             }
         }
     }
-
 }
